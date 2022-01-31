ce57733e39c665fb86b69b487b45946584b034d3
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2022-01-31 12:02:25 -0500

    Add infinite scrool to ImageList

--- pixel/components/ImageList.js
+++ pixel/components/ImageList.js
@@ -2,26 +2,42 @@ import React, { useState, useRef, useEffect, useCallback } from 'react';
 import useInView from 'react-cool-inview'
 import Gallery from 'react-photo-gallery'
 import Modal from 'react-modal'
+import Image from 'next/image'
+import { v4 as uuidv4 } from 'uuid'
 import { getImages } from '../api/apiCalls';
 
+const customModalStyle = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    maxHeight: '700px'
+  },
+};
+  
+//Binding modal to our app
+Modal.setAppElement("#__next");
 
 const ImageList = () => {
   const [images, setImages] = useState([]);
-  const [currentImageSrc, setCurrentImageSrc] = useState(0); 
+  const [currentImage, setCurrentImage] = useState({}); 
   const [modalIsOpen, setIsOpen] = useState(false);
   
   const loadImages = async () => {
     try {
       const response = await getImages();
-      console.log(response.data); 
       const newImages = [];
-      response.data.forEach(image => {
-          newImages.push({
-              src : image.src,
-              height : image.height,
-              width : image.width,
-              rawSrc : image.rawImageUrl
-          })
+      response.data.forEach(image => {    
+        newImages.push({
+            key : uuidv4(),
+            src : image.src,
+            height : image.height,
+            width : image.width,
+            raw_image_src: image.rawImageUrl
+        })
       });
       setImages(prevImages => [...prevImages, ...newImages]);
     } catch (error) {
@@ -31,34 +47,35 @@ const ImageList = () => {
   
   useEffect(async () => loadImages(), []);
 
-//   const { observe } = useInView({
-//       rootMargin: "50px 0px",
-//       // When the last item comes to the viewport
-//       onEnter: async ({ unobserve }) => {
-//           // Pause observe when loading the data 
-//           unobserve();
-//           // Load more data
-//           console.log("loading more data");
-//           await loadImages();
-//       },
-//   });
+  const { observe } = useInView({
+      rootMargin: "50px 0px",
+      // When the last item comes to the viewport
+      onEnter: async ({ unobserve }) => {
+          // Pause observe when loading the data 
+          unobserve();
+          // Load more data
+          console.log("loading more data");
+          await loadImages();
+          observe();
+      },
+  });
   
   const openModal = useCallback((event, { photo, index }) => {
-    setCurrentImageSrc(photo.rawSrc);
+    setCurrentImage(photo);
     setIsOpen(true);
   }, []);
 
   const closeModal = () => {
-    setCurrentImageSrc("");
     setIsOpen(false);
   }
 
   return (
     <div>
-      {images[0] && <Gallery photos={images} direction={"column"} onClick={openModal}/>}
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-        <img src={currentImageSrc} />
+      {images[0] && <Gallery photos={images} direction={"column"} onClick={openModal} margin={5}/>}
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customModalStyle}>
+        <Image src={currentImage.raw_image_src} width={currentImage.width} height={currentImage.height} placeholder="blur" blurDataURL={currentImage.src}/>
       </Modal>
+      <div ref={observe}></div>
     </div>
   );
 };
